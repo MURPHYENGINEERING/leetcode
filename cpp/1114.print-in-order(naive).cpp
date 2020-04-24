480acf3c4790c0a0f75c36f72242d5f39f7b7794
MURPHYENGINEERING <matt@murphyengineer.com>
2020-04-24 02:42:09 -0700

    Update 1114.print-in-order(naive).cpp

--- cpp/1114.print-in-order(naive).cpp
+++ cpp/1114.print-in-order(naive).cpp
@@ -58,8 +58,6 @@ private:
       // std::unique_lock facilitates manual locking and unlocking. It's movable,
       // but not copyable, so it can't be accidentally duplicated and used to deadlock.
       std::unique_lock lock(stoplight_mtx);
-      // Naive to the implementation of std::condition_variable::wait,
-      // we run our own predicate loop.
       stoplight.wait(lock, [=] { return released_count == count; });
 
       then();
