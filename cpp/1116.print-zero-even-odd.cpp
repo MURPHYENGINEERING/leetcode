a533b4d94dbf1eab1aa1265a4d8bb304a449230d
MURPHYENGINEERING <matt@murphyengineer.com>
2020-04-29 16:22:23 -0700

    Create 1116.print-zero-even-odd.cpp

---
+++ cpp/1116.print-zero-even-odd.cpp
@@ -0,0 +1,91 @@
+// The zero thread counts n to limit and delegates to the appropriate thread
+// We want to avoid lock contention as much as possible so the zero thread will
+// only notify the thread whose turn it is, instead of notifying all and having the
+// threads predicate on the condition (they still predicate for spurious wakes).
+
+#include <mutex>
+#include <condition_variable>
+
+
+class ZeroEvenOdd final {
+public:
+  ZeroEvenOdd(const int n) : turn_limit_(n) {
+  }
+
+  void zero(function<void(int)> print_number) {
+    std::unique_lock lock(turn_mtx_);
+    
+    while (!should_quit()) {
+      print_number(0);
+      
+      if (is_turn(Turn::Even)) {
+        even_barrier_.notify_one();
+      } else {
+        odd_barrier_.notify_one();
+      }
+      
+      // Since C++20 we can wait on the atomic changing
+      // Release lock so the turn can proceed
+      zero_barrier_.wait(lock, [&] { return turn_is_completed; });
+      
+      ++n_;
+      turn_is_completed = false;
+    }
+    
+    // should_quit is true so release both threads
+    even_barrier_.notify_one();
+    odd_barrier_.notify_one();
+  }
+
+  void even(function<void(int)> print_number) {
+    take_turns_repeatedly(Turn::Even, even_barrier_, print_number);
+  }
+
+  void odd(function<void(int)> print_number) {
+    take_turns_repeatedly(Turn::Odd, odd_barrier_, print_number);
+  }
+  
+private:
+  enum Turn { Even, Odd };
+  // Each thread checks this condition so encapsulate the logic
+  bool is_turn(Turn turn) const {
+    return !turn_is_completed && ((turn == Turn::Even) ? (n_ % 2 == 0) : (n_ % 2 != 0));
+  }
+  // Name this logic to show intent in the thread conditions
+  bool should_quit() const {
+    return n_ > turn_limit_;
+  }
+  
+  void take_turns_repeatedly(
+    const Turn turn_to_take, 
+    std::condition_variable& barrier,
+    const std::function<void(int)> take_turn
+  ) {
+    std::unique_lock lock(turn_mtx_);
+
+    while (!should_quit()) {
+      // Release lock so zero can delegate
+      barrier.wait(lock, [&] { return should_quit() || is_turn(turn_to_take); });
+      if (should_quit()) return;
+      
+      take_turn(n_);
+      
+      // Since C++20 we only need to set an atomic<bool> to notify
+      turn_is_completed = true;
+      zero_barrier_.notify_one();
+    }
+  }
+  
+  
+  const int turn_limit_;
+  // We output the turn number and the first turn outputs 1 so there is no zeroth turn.
+  int n_ = 1;  
+  bool turn_is_completed{false};
+  
+  std::mutex turn_mtx_;
+  
+  std::condition_variable zero_barrier_;
+  
+  std::condition_variable even_barrier_;
+  std::condition_variable odd_barrier_;
+};
